edc3d1b52bf19dcc9f56add9721ae79e9c472259
krzysztof <krzszk96@gmail.com>
2022-08-02 21:40:57 +0200

    changes v1

--- app.js
+++ app.js
@@ -1,58 +1,55 @@
 
-var honeyModal = document.getElementById("honeyModal"); // Get the modal
-var btnh = document.getElementById("honeyBtn"); // Get the button that opens the modal
+var meatModal = document.getElementById("meatModal"); // Get the modal
+var btnh = document.getElementById("meatBtn"); // Get the button that opens the modal
 
-// When the user clicks the button, open the modal 
 btnh.onclick = function() {
+  meatModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  meatModal.style.display = "none";
+}
+
+//wine
+var wineModal = document.getElementById("wineModal"); 
+var btns = document.getElementById("wineBtn"); 
+btns.onclick = function() {
+  wineModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[1];
+span.onclick = function() {
+  wineModal.style.display = "none";
+}
+
+//juice
+var rasberryModal = document.getElementById("rasberryModal"); 
+var btnp = document.getElementById("aronBtn"); 
+btnp.onclick = function() {
+  rasberryModal.style.display = "block";
+}
+var span = document.getElementsByClassName("close")[2];
+span.onclick = function() {
+  rasberryModal.style.display = "none";
+}
+
+//cherries
+var honeyModal = document.getElementById("honeyModal"); 
+var btnc = document.getElementById("cherryBtn"); 
+btnc.onclick = function() {
   honeyModal.style.display = "block";
 }
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[3];
 span.onclick = function() {
   honeyModal.style.display = "none";
 }
 
-//strawberries
-var strawModal = document.getElementById("strawModal"); 
-var btns = document.getElementById("strawBtn"); 
-btns.onclick = function() {
-  strawModal.style.display = "block";
-}
-var span = document.getElementsByClassName("close")[1];
-span.onclick = function() {
-  strawModal.style.display = "none";
-}
-
-//plums
-var plumModal = document.getElementById("plumModal"); 
-var btnp = document.getElementById("plumBtn"); 
-btnp.onclick = function() {
-  plumModal.style.display = "block";
-}
-var span = document.getElementsByClassName("close")[2];
-span.onclick = function() {
-  plumModal.style.display = "none";
-}
-
-//cherries
-var CherryModal = document.getElementById("cherryModal"); 
-var btnc = document.getElementById("cherryBtn"); 
-btnc.onclick = function() {
-  CherryModal.style.display = "block";
-}
-var span = document.getElementsByClassName("close")[3];
-span.onclick = function() {
-  CherryModal.style.display = "none";
-}
-
 //last
-var lastModal = document.getElementById("lastModal"); 
+var fishModal = document.getElementById("fishModal"); 
 var btnc = document.getElementById("honey"); 
 btnc.onclick = function() {
-  lastModal.style.display = "block";
+  fishModal.style.display = "block";
 }
 var span = document.getElementsByClassName("close")[4];
 span.onclick = function() {
-  lastModal.style.display = "none";
+  fishModal.style.display = "none";
 }
